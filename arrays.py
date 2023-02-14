610fe1ea82d4a6a60119c5acfd97d14d95e69faf
Abdiwilly <abdiwillyhassan@gmail.com>
2023-02-14 11:16:05 +0300

    arrays.py

---
+++ arrays.py
@@ -0,0 +1,23 @@
+#num=[9,10,2,5]
+#x=num[0]
+#print(x)
+
+#num=[10,20,50,6]
+#num[2]=9
+#print(num)
+
+#cars=["porshe,""BMW","Jeep"]
+#cars.append("Toyota")
+#print(cars)
+
+#num=[9,11,15,16]
+#num.pop(2)
+#print(num)
+
+#num=[3,5,6,77,8,9]
+#for x in num:
+  #  print(x)
+
+num=[3,56,32,21,91]
+for x in range(3):
+ print(num[x])
---
+++ strings.py
@@ -0,0 +1,40 @@
+#name= "climate"
+#print(len(name))
+
+#name="climate"
+#for letter  in name:
+    #print(letter)
+
+#first_name="Hassan"
+#second_name="Abdiwilly"
+#full_name=first_name + " " + second_name   
+#print(full_name)
+
+#name=input("Enter your name:")
+#print("hello" + " " + name)
+
+#name="Hassan"
+#name1="Abdiwilly"
+#print(name)
+#print(name1)
+# 
+
+#name="hassan"
+#print(name.upper())
+
+
+#password="string3"
+#print(password.isalpha())
+
+#if password.isalpha():
+ #   print("invalid password")
+#else:
+ #   print("valid password")
+
+#name=input("USERNAME?")
+#print("Hello" + " " + name.upper())
+
+name="x1234edwin5776"
+print(name[5:10])
+email="bob123@gmail.com"
+print(email)
